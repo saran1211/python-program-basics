5ad34a49e44af97ee529deb4732fd835abf3ba7b
saran1211 <36408495+saran1211@users.noreply.github.com>
2018-03-01 17:59:10 +0530

    primeornot
    number is prime or not

--- primeornot.py
+++ primeornot.py
@@ -1,14 +1,12 @@
+num=13
 if num > 1:
-   # check for factors
-   for i in range(2,num):
+    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
+           print(num,"it is not a prime num")
            print(i,"times",num//i,"is",num)
            break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
+    else:
+       print(num,"is a prime num")
+
 else:
-   print(num,"is not a prime number")
+   print(num,"is a prime num")
