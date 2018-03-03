5e599984eb17d2d77d5279742f364f0c59fe8aef
saran1211 <36408495+saran1211@users.noreply.github.com>
2018-03-03 17:15:27 +0530

    oddnum
    print odd number between given range

--- oddnum.py
+++ oddnum.py
@@ -1,5 +1,5 @@
-lower=int(input("Enter the lower limit for the range:"))
-upper=int(input("Enter the upper limit for the range:"))
+lower=int(10)
+upper=int(25)
 for i in range(lower,upper+1):
     if(i%2!=0):
         print(i)
